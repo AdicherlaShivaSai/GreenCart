ac1d38a82ea2b3614dffc93c6971666710ca37c8
AdicherlaShivaSai <shivasai7601@gmail.com>
2025-06-19 11:39:52 +0530

    secure updated

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -11,7 +11,7 @@ export const sellerLogin = async(req, res)=>{
 
         res.cookie('sellerToken', token,{
             httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
-            secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+            secure: true, // Use secure cookies in production
             sameSite: 'None', // Helps prevent CSRF attacks
             maxAge: 7 * 24 * 60 * 60 * 1000 // Cookie expiration time (7 days)
         })
@@ -43,7 +43,7 @@ export const sellerLogout = async(req, res)=>{
     try{
         res.clearCookie('sellerToken',{
             httpOnly:true,
-            secure: process.env.NODE_ENV ==='production',
+            secure: true,
             sameSite: 'None',
         })
         return res.json({success: true, message: "Logged Out"})
